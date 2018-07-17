851e9933bd1607ddb333538af8fdfcc13657ae48
arbiben <ben.a@ibm.com>
2018-07-17 16:35:41 -0500

    testcases

--- app/src/main/java/com/example/ben/splitpay/MainActivity.java
+++ app/src/main/java/com/example/ben/splitpay/MainActivity.java
@@ -7,7 +7,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("Hello Shalom-------------------------");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        Person p = new Person("Ben", "Arbib", new Accounts());
+//        BillingItem b = new BillingItem("Apple", 30.0);
+//        Bill bill = new Bill();
+//        System.out.println("Hello Shalom");
     }
 }
