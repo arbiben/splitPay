d9704b2af8156c0310493f0ed6e35507b9c94866
Ben Arbib <ba2490@columbia.edu>
2018-07-23 16:18:12 -0400

    utin test for linkitems class

---
+++ app/src/test/java/com/example/ben/splitpay/LinkItemsTest.java
@@ -0,0 +1,29 @@
+package com.example.ben.splitpay;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LinkItemsTest {
+    LinkItems linkItems;
+    @Before
+    public void setUp() throws Exception {
+        linkItems = new LinkItems();
+    }
+
+    @Test
+    public void linkItems() {
+        // person id and item id are linked
+        linkItems.linkItems(1, 5);
+        linkItems.linkItems(1, 1);
+        linkItems.linkItems(1, 7);
+        linkItems.linkItems(2, 3);
+        linkItems.linkItems(2, 12);
+        linkItems.linkItems(3, 6);
+        linkItems.linkItems(4, 4);
+        int personID = linkItems.getPersonLinkedToBillingItem(12);
+        assertEquals(personID, 2);
+    }
+}
