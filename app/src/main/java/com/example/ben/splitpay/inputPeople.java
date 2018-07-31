948172abd86e390008d283d13009ed713482bd86
Ben Arbib <ba2490@columbia.edu>
2018-07-31 15:55:05 -0500

    trying to transfer data from inputBillingItems to inputPeople

--- app/src/main/java/com/example/ben/splitpay/inputPeople.java
+++ app/src/main/java/com/example/ben/splitpay/inputPeople.java
@@ -1,5 +1,21 @@
 package com.example.ben.splitpay;
 
-public class inputPeople {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import java.util.ArrayList;
 
+public class inputPeople extends AppCompatActivity {
+    ArrayList<String> names;
+    ArrayList<Double> prices;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_people);
+
+        Bundle bundle = getIntent().getExtras();
+        names = bundle.getStringArrayList("billingNames");
+        prices = (ArrayList<Double>) getIntent().getSerializableExtra("billingPrices");
+
+    }
 }
