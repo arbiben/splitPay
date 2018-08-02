0c3b753ce7724110b1650a4a33af8c3e8ab97e71
Ben Arbib <ba2490@columbia.edu>
2018-08-01 23:07:23 -0500

    started activity to link people and billing items

--- app/src/main/java/com/example/ben/splitpay/link_people_to_bill.java
+++ app/src/main/java/com/example/ben/splitpay/link_people_to_bill.java
@@ -3,11 +3,22 @@ package com.example.ben.splitpay;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class link_people_to_bill extends AppCompatActivity {
+    private ArrayList<String> people;
+    private ArrayList<String> billingItems;
+    private ArrayList<Double> prices;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_link_people_to_bill);
+
+        Bundle bundle = getIntent().getExtras();
+        people = bundle.getStringArrayList("names");
+        billingItems = bundle.getStringArrayList("billingItemNames");
+        prices = (ArrayList<Double>) getIntent().getSerializableExtra("prices");
+
     }
 }
