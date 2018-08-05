50897ee54dd9b9d48fd68efaf7c915eeb5fcf739
Ben Arbib <ba2490@columbia.edu>
2018-08-05 02:04:04 -0500

    done with first version of link people to bill

--- app/src/main/java/com/example/ben/splitpay/show_totals.java
+++ app/src/main/java/com/example/ben/splitpay/show_totals.java
@@ -2,12 +2,16 @@ package com.example.ben.splitpay;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.HashMap;
 
 public class show_totals extends AppCompatActivity {
-
+    public static final String TAG = "show totals!";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_totals);
+        HashMap<String, Person> p = (HashMap<String, Person>) getIntent().getSerializableExtra("people");
     }
 }
